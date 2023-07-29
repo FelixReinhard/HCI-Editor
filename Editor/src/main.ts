1cdb77287a9c0dda93ddda2baa1376f289135a6e
= <=>
2023-07-29 19:06:03 +0200

    Camera controls working, scaffolding for btn selection,

--- Editor/src/main.ts
+++ Editor/src/main.ts
@@ -1,6 +1,9 @@
 import './style.css'
 
 import * as Three from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+
+// Positions are in cm
 
 const scene = new Three.Scene();
 const camera = new Three.PerspectiveCamera(80, (window.innerWidth * 0.6) / window.innerHeight, 0.1, 1000);
@@ -12,23 +15,49 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth * 0.6, window.innerHeight);
 camera.position.setZ(30);
 
-const test = new Three.TorusGeometry(10, 3 ,16, 100);
-const test_mat = new Three.MeshBasicMaterial({color: 0xFF0000});
-const t = new Three.Mesh(test, test_mat);
+// const test = new Three.TorusGeometry(10, 3 ,16, 100);
+// const test_mat = new Three.MeshBasicMaterial({color: 0xFF0000});
+// const t = new Three.Mesh(test, test_mat);
+//
+const geometry = new Three.CircleGeometry( 1, 20 ); 
+const material = new Three.MeshBasicMaterial( { color: 0xffff00 } ); 
+const circle = new Three.Mesh( geometry, material ); 
+circle.rotateX(-Math.PI / 2);
+scene.add( circle );
 
-scene.add(t);
-
-const gridHelper = new Three.GridHelper( 10, 10);
+const orbitControl = new OrbitControls(camera, renderer.domElement);
+//
+// scene.add(t);
+// Grid 
+const gridHelper = new Three.GridHelper( 100, 100);
 scene.add( gridHelper );
-
-// camera.position.x+=Math.sin(camera.rotationy)*3;
-// camera.position.z+=Math.cos(camera.rotationy)*3;
-// tempVector.copy(target).y+=window.innerHeight; // the += is optional
-// camera.lookAt( tempVector );
 
 function animate() {
   requestAnimationFrame(animate);
+  orbitControl.update();
   renderer.render(scene, camera);
 }
+
+// check keyboard input to move OrbitControls 
+document.addEventListener("keydown", function(event) {
+  switch (event.key) {
+    case "w":
+      orbitControl.target.z += 1;
+      break;
+    case "s":
+      orbitControl.target.z -= 1;
+      break;
+    case "d":
+      orbitControl.target.x += 1;
+      break;
+    case "a":
+      orbitControl.target.x -= 1;
+      break;
+    default:
+      break;
+  }
+
+  circle.position.set(orbitControl.target.x, 0, orbitControl.target.z);
+});
 
 animate();
