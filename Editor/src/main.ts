d3ce6ecf8e2442899d1ef254e1e816c083dce902
= <=>
2023-08-01 17:28:29 +0200

    added movement

--- Editor/src/main.ts
+++ Editor/src/main.ts
@@ -3,8 +3,11 @@ import './style.css'
 import * as Three from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
-// Positions are in cm
+// State 
+var mode: String = "move"
+var mouse_mode: String = "drag"; // drag, put, scale
 
+// Positions are in cm
 const scene = new Three.Scene();
 const camera = new Three.PerspectiveCamera(80, (window.innerWidth * 0.6) / window.innerHeight, 0.1, 1000);
 const renderer = new Three.WebGLRenderer({
@@ -15,14 +18,11 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth * 0.6, window.innerHeight);
 camera.position.setZ(30);
 
-// const test = new Three.TorusGeometry(10, 3 ,16, 100);
-// const test_mat = new Three.MeshBasicMaterial({color: 0xFF0000});
-// const t = new Three.Mesh(test, test_mat);
-//
 const geometry = new Three.CircleGeometry( 1, 20 ); 
 const material = new Three.MeshBasicMaterial( { color: 0xffff00 } ); 
 const circle = new Three.Mesh( geometry, material ); 
 circle.rotateX(-Math.PI / 2);
+circle.scale.set(0.5, 0.5, 1)
 scene.add( circle );
 
 const orbitControl = new OrbitControls(camera, renderer.domElement);
@@ -38,20 +38,50 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+function switch_mode(new_mode: String) {
+  mode = new_mode
+  switch (mode) {
+    case "editing":
+      orbitControl.enablePan = false
+      orbitControl.enableRotate = false
+      camera.position.set(0, 30, 0);
+      camera.rotation.set(0, -Math.PI / 2, 0)
+      break;
+
+    default:
+      break;
+  }
+}
+
+// Camera movement 
+function moveY(dir) {
+  orbitControl.target.z += dir;
+  camera.position.z += dir;
+}
+
+function moveX(dir) {
+  orbitControl.target.x += dir;
+  camera.position.x += dir;
+}
+
 // check keyboard input to move OrbitControls 
 document.addEventListener("keydown", function(event) {
   switch (event.key) {
+    case "e":
+      // switch to editing mode
+      switch_mode("editing")
+      break;
     case "w":
-      orbitControl.target.z += 1;
+      moveY(-1)
       break;
     case "s":
-      orbitControl.target.z -= 1;
+      moveY(1)
       break;
     case "d":
-      orbitControl.target.x += 1;
+      moveX(1)
       break;
     case "a":
-      orbitControl.target.x -= 1;
+      moveX(-1)
       break;
     default:
       break;
@@ -60,4 +90,29 @@ document.addEventListener("keydown", function(event) {
   circle.position.set(orbitControl.target.x, 0, orbitControl.target.z);
 });
 
+var last_pos = {'x': 0, 'y': 0}
+var mouse_pressed = 0;
+document.addEventListener("mouseup", function() {
+  --mouse_pressed
+  last_pos = {'x': 0, 'y': 0}
+})
+
+document.addEventListener("mousedown", function(event) {
+  ++mouse_pressed
+  last_pos = {'x': event.clientX, 'y': event.clientY}
+})
+
+// editing mode drag camera
+document.addEventListener("mousemove", function(event) {
+  if (mouse_pressed && mode == "editing") {
+    const dir = {'x': event.clientX - last_pos.x, 'y': event.clientY - last_pos.y}
+    moveX(-dir.x * 0.1 * orbitControl.zoom0)
+    moveY(-dir.y * 0.1 * orbitControl.zoom0)
+    last_pos = {'x': event.clientX, 'y': event.clientY}
+  }
+})
+
 animate();
+
+
+
