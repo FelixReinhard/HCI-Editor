041ad01ff9a300d5f8d87f49fc3eff0483237c80
= <=>
2023-08-21 17:15:27 +0200

    export new primitives

--- Editor/src/export.ts
+++ Editor/src/export.ts
@@ -4,29 +4,115 @@ import { CollisionBox } from "./collision.ts";
 
 const DEF_SIZE = [500, 500];
 
+const export_functions = {
+  "basic1d": basic1D,
+  "right1d": right1D,
+  "basic2d": basic2D,
+  "basic1d_chained": basic1d_chained,
+  "full1d": full1D,
+  "slope71d": slope71D,
+  "slope1d": slope1D,
+  "angle1d": angle1D,
+}
+
 export function export_cells(cells: Cell[], format: string) {
   const writer: Writer = format == "svg" ? new SvgWriter(DEF_SIZE[0], DEF_SIZE[1]) : new DXFWriter();
   for (const cell of cells) {
-    switch (cell.type) {
-      case "basic1d":
-        basic1D([cell.position.x + DEF_SIZE[0]/2.0, (cell.position.z + DEF_SIZE[1]/2.0)], cell.amplitude, cell.width, cell.coll, writer);
-        break;
-      case "right1d":
-        right1D([cell.position.x + DEF_SIZE[0]/2.0, (cell.position.z + DEF_SIZE[1]/2.0)], cell.amplitude, cell.width, cell.coll, writer);
-        break;
-      case "basic2d":
-        basic2D([cell.position.x + DEF_SIZE[0]/2.0, (cell.position.z + DEF_SIZE[1]/2.0)], cell.amplitude, cell.width, writer);
-        break;
-      case "chained_basic_1d":
-        basic1d_chained([cell.position.x + DEF_SIZE[0]/2.0, (cell.position.z + DEF_SIZE[1]/2.0)], cell.amplitude, cell.width, writer, cell.meta_data);
-        break;
-      default:
-        break;
+    if (cell.type in export_functions) {
+      export_functions[cell.type]([cell.position.x + DEF_SIZE[0]/2.0, (cell.position.z + DEF_SIZE[1]/2.0)], cell.amplitude, cell.width, cell.coll, writer);
+    } else if (cell.type == "basic1d_chained"){
+      basic1d_chained([cell.position.x + DEF_SIZE[0]/2.0, (cell.position.z + DEF_SIZE[1]/2.0)], cell.amplitude, cell.width, writer, cell.meta_data);
+    } else {
+      console.log(`Cant export ${cell.type}`);
     }
   }
   writer.save();
 }
 
+function angle1D(position: number[], amplitude: number, width: number,collisions: CollisionBox[], writer: Writer) {
+  const f = formula(amplitude, width, c1); 
+
+  const b = f[1];
+  const a = f[0];
+
+
+  const yOffset = -(a * Math.sin(20 * (Math.PI / 180.0))) / Math.sin(70 * (Math.PI / 180.0));
+  position[1] += yOffset;
+  // ...quad(
+  //   [DEFAULT_SIZE*2, 0, 0], [DEFAULT_SIZE*2, 0, b], [DEFAULT_SIZE*2 + a, 0, yOffset], [DEFAULT_SIZE*2 + a, 0, yOffset + b]
+  // ),
+  // ...quad(
+  //   [DEFAULT_SIZE*3 + a, 0, yOffset], [DEFAULT_SIZE*3 + a, 0, yOffset + b], [DEFAULT_SIZE*3 + 2*a, 0, 0], [DEFAULT_SIZE*3 + 2*a, 0, b]
+  // ),
+
+  writer.rect(position[0], position[1], DEFAULT_SIZE, b);
+  writer.path([
+    [position[0] + DEFAULT_SIZE*2, position[1] - b],
+    [position[0] + DEFAULT_SIZE*2 + a, position[1] - yOffset - b],
+    [position[0] + DEFAULT_SIZE*2 + a, position[1] - yOffset],
+    [position[0] + DEFAULT_SIZE*2, position[1]],
+  ]);
+  writer.path([
+    [position[0] + DEFAULT_SIZE*3 + a, position[1] - b - yOffset],
+    [position[0] + DEFAULT_SIZE*3 + a, position[1] - yOffset],
+    [position[0] + DEFAULT_SIZE*3 + 2*a, position[1]],
+    [position[0] + DEFAULT_SIZE*3 + 2*a, position[1] - b],
+  ]);
+  writer.rect(position[0] + DEFAULT_SIZE*4 + 2*a , position[1], DEFAULT_SIZE, b);
+}
+
+function slope1D(position: number[], amplitude: number, width: number,collisions: CollisionBox[], writer: Writer) {
+  const f = formula(amplitude, width, c1); 
+
+  const b = f[1];
+  const a = f[0];
+
+  writer.rect(position[0], position[1], DEFAULT_SIZE, b);
+  writer.path([
+    [position[0] + DEFAULT_SIZE*2, position[1] - b],
+    [position[0] + DEFAULT_SIZE*2 + a, position[1] + b/2.0 - b],
+    [position[0] + DEFAULT_SIZE*2, position[1]]
+  ]);
+  writer.path([
+    [position[0] + DEFAULT_SIZE*3 + a, position[1] + b/2.0 - b],
+    [position[0] + DEFAULT_SIZE*3 + 2*a, position[1] - b],
+    [position[0] + DEFAULT_SIZE*3 + 2*a, position[1]],
+  ]);
+  writer.rect(position[0] + DEFAULT_SIZE*4 + 2*a , position[1], DEFAULT_SIZE, b);
+}
+
+function slope71D(position: number[], amplitude: number, width: number,collisions: CollisionBox[], writer: Writer) {
+  const f = formula(amplitude, width, c1); 
+
+  const b = f[1];
+  const a = f[0];
+
+  writer.rect(position[0], position[1], DEFAULT_SIZE, b);
+  writer.path([
+    [position[0] + DEFAULT_SIZE*2, position[1] - b],
+    [position[0] + DEFAULT_SIZE*2 + a, position[1] + b*.15 - b],
+    [position[0] + DEFAULT_SIZE*2 + a, position[1] + b*.85 - b],
+    [position[0] + DEFAULT_SIZE*2, position[1]]
+  ]);
+  writer.path([
+    [position[0] + DEFAULT_SIZE*3 + a, position[1] + b*.15 - b],
+    [position[0] + DEFAULT_SIZE*3 + 2*a, position[1] - b],
+    [position[0] + DEFAULT_SIZE*3 + 2*a, position[1]],
+    [position[0] + DEFAULT_SIZE*3 + a, position[1] + b*.85 - b],
+  ]);
+  writer.rect(position[0] + DEFAULT_SIZE*4 + 2*a , position[1], DEFAULT_SIZE, b);
+}
+function full1D(position: number[], amplitude: number, width: number,collisions: CollisionBox[], writer: Writer) {
+  const f = formula(amplitude, width, c1); 
+
+  const b = f[1];
+  const a = f[0];
+
+    //...rect(a*2 + DEFAULT_SIZE, b, [DEFAULT_SIZE*2, 0]),
+  writer.rect(position[0], position[1], DEFAULT_SIZE, b);
+  writer.rect(position[0] + DEFAULT_SIZE*2, position[1], a*2 + DEFAULT_SIZE, b);
+  writer.rect(position[0] + DEFAULT_SIZE*4 + 2*a , position[1], DEFAULT_SIZE, b);
+}
 function basic1d_chained(position: number[], amplitude: number, width: number, writer: Writer, data: string[]) {
   const f = formula(amplitude, width, c1); 
 
@@ -78,7 +164,7 @@ function right1D(position: number[], amplitude: number, width: number,collisions
   writer.rect(position[0] + DEFAULT_SIZE*4 + 2*a , position[1], DEFAULT_SIZE, b);
 }
 
-function basic2D(position: number[], amplitude: number, width: number, writer: SvgWriter) {
+function basic2D(position: number[], amplitude: number, width: number, collisions: CollisionBox[],  writer: Writer) {
 
   const f = formula(amplitude, width, c2); 
 
